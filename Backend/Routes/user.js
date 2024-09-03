f50cfce8559994857135e8d277a22ab0ee1c6512
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2024-09-03 00:01:45 -0900

    user controller

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -11,6 +11,6 @@ router.put("/:id",authenticate, restrict(['patient']), updateUser);
 router.delete("/:id",authenticate, restrict(['patient']), deleteUser);
 router.get("/profile/me",authenticate, restrict(['patient']), getUserProfile);
 router.delete("/appointments/my-appointments",authenticate, restrict(['patient']), getMyAppointments);
-router.get("appointments/my-appointments",authenticate, restrict(['patient']), getMyAppointments);
+router.get("/appointments/my-appointments",authenticate, restrict(['patient']), getMyAppointments);
 
 export default router; 
