fae8125c2ce8f4a5d2e5839749b3b17fbc19b950
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2024-09-03 00:01:15 -0900

    main file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from "react-dom/client";
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css"
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <AuthContextProvider>
+    <ToastContainer 
+      theme='dark' 
+      position = 'top-right' 
+      autoClose = {3000} 
+      closeOnClick 
+      pauseOnHover = {false}/>
+      <App />
+    </AuthContextProvider>
+
+    </BrowserRouter>
+  </React.StrictMode>,
 )
