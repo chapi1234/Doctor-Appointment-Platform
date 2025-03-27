e02ff730c76932fc0ebae0cc0662e94ab0b50bd9
metasebiyaw asfaw <metasebiyawasfaw@gmail.com>
2025-03-27 23:54:26 +0300

    update

--- Frontend/src/utils/convertTime.js
+++ Frontend/src/utils/convertTime.js
@@ -1,24 +1,21 @@
-const converTime = time =>{
-    const timeParts= time.split(":")
+const convertTime = (time) => {
+    const timeParts = time.split(":");
     let hours = parseInt(timeParts[0]);
     const minutes = parseInt(timeParts[1]);
-
-    let meridian = 'am'
-
-    if (hours> 12){
-        meridian='pm';
-
-        if (hours< 12){
-            meridian='am';
-
+  
+    let meridian = 'AM';
+  
+    if (hours >= 12) {
+      meridian = 'PM';
+      if (hours > 12) {
+        hours -= 12; // Convert to 12-hour format
+      }
+    } else if (hours === 0) {
+      hours = 12; // Midnight case
     }
-}
-return (
-    hours.toString().padStart(2), ":,",
-    minutes.toString().padStart(2, "0"),
-    " ",
-    meridian
-);
-};
-
-export default converTime;
+  
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${meridian}`;
+  };
+  
+  export default convertTime;
+  
